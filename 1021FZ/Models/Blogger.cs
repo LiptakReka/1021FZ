146aed741cd2467023f15f0b16e09aa69ad8067b
LiptakReka <liptakr@kkszki.hu>
2024-10-21 08:22:07 +0200

    Models folder created

---
+++ 1021FZ/Models/Blogger.cs
@@ -0,0 +1,12 @@
+﻿namespace _1021FZ.Models
+{
+    public class Blogger
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Status { get; set; }
+        public DateTime dateat { get; set; }
+    }
+
+}
